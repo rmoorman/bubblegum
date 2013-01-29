5f6dd5d7af5c028d24809e2143b081a1c4aba0c6
Paul Khusainov <alvelcom@gmail.com>
2013-01-29 18:12:31 +0400

    Added get_pool, round_robin and tests to poolpool

--- src/poolpool.erl
+++ src/poolpool.erl
@@ -39,7 +39,7 @@ init([Pools, Config]) ->
     Active = queue:new(),
     Down   = queue:new(),
     Backup = queue:new(),
-    Table  = ets:new(poolpool_wrk2boy, []),
+    Table  = ets:new(poolpool_wrk2boy, [public]),
     {ok, Sup} = poolpool_sup:start_link(?UNDERPOOL, []),
     State = #state{
             supervisor = Sup,
@@ -56,17 +56,38 @@ init([Pools, Config]) ->
     New2State = populate(BackupList, backup, New1State),
     {ok, New2State}.
 
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+handle_call(get_pool, _From, State) ->
+    #state{
+        active = Active,
+        backup = Backup,
+        wrk2boy = EtsId
+        } = State,
+    {ActPid, NewActive} = round_robin(Active),
+    {BoyPid, NewBack} = if
+        ActPid == empty ->
+            round_robin(Backup);
+        true ->
+            {ActPid, Backup}
+    end,
+    {reply, {BoyPid, EtsId}, State#state{active = NewActive, backup = NewBack}}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+handle_cast({set_down, Pool}, State) ->
+    Fun = fun (E) -> Pool /= E end,
+    IsMember = queue:member(Pool, State#state.active),
+    if
+         IsMember ->
+            NewActive = queue:filter(Fun, State#state.active),
+            NewDown   = queue:in(Pool, State#state.down),
+            {noreply, State#state{active = NewActive, down = NewDown}};
+        true ->
+            {noreply, State}
+    end.
 
 handle_info(_Info, State) ->
     {noreply, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, State) ->
+    ets:delete(State#state.wrk2boy),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -86,4 +107,108 @@ run_pool({PoolConf, WorkerConf}, Sup) ->
     Pid = supervisor:start_child(Sup, poolboy:child_spec(PoolConf, WorkerConf)),
     link(Pid),
     Pid.
+
+round_robin(Queue) ->
+    {Ans, Q1} = queue:out(Queue),
+    if
+        Ans == empty ->
+            {empty, Queue};
+        true ->
+           {value, Item} = Ans, 
+            Q2 = queue:in(Item, Q1),
+            {Ans, Q2}
+    end.
+
+
+%%%===================================================================
+%%% Tests
+%%%===================================================================
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+round_robin_test() ->
+    Emt = queue:new(),
+    ?assert({empty, Emt} == round_robin(Emt)),
+    Oneel = queue:in(1, Emt),
+    {{value, 1}, Q1} = R1 = round_robin(Oneel),
+    {{value, 1}, Q2} = R2 = round_robin(Q1),
+    FullQueue = lists:foldl(fun (El, Acc) ->
+                queue:in(El, Acc)
+        end, Q2, lists:seq(2, 100)),
+
+    S = lists:seq(1, 100),
+    lists:foldl(fun (El, Acc) ->
+                io:format("Next item is ~B~n", [El]),
+                {{value, El}, Out} = round_robin(Acc),
+                Out
+        end, FullQueue, S ++ S ++ S),
+    ok.
+    
+set_down_test() ->
+    Emt = queue:new(),
+    List = lists:seq(1, 100),
+    Queue = queue:from_list(List),
+    State = #state{
+            active = Queue,
+            down = Emt,
+            backup = Emt
+            },
+    {noreply, State} = handle_cast({set_down, 4242}, State),
+    {noreply, State1} = handle_cast({set_down, 42}, State),
+    ?assert(false =:= queue:member(42, State1#state.active)),
+    ?assert(true  =:= queue:member(42, State1#state.down)),
+    {noreply, State1} = handle_cast({set_down, 42}, State1),
+    ok.
+
+get_pool_test() ->
+    Emt = queue:new(),
+    Active = queue:from_list([1, 2, 3, 4]),
+    Backup = queue:from_list([5, 6, 7]),
+    StateA = #state{
+            active = Active,
+            down   = Emt,
+            backup = Emt
+            },
+    {reply, Ans1, StateA1} = handle_call(get_pool, undefined, StateA),
+    {{value, 1}, _} = Ans1,
+    {reply, Ans2, StateA2} = handle_call(get_pool, undefined, StateA1),
+    {{value, 2}, _} = Ans2,
+    {reply, Ans3, StateA3} = handle_call(get_pool, undefined, StateA2),
+    {{value, 3}, _} = Ans3,
+    {reply, Ans4, StateA4} = handle_call(get_pool, undefined, StateA3),
+    {{value, 4}, _} = Ans4,
+    {reply, Ans5, StateA5} = handle_call(get_pool, undefined, StateA4),
+    {{value, 1}, _} = Ans5,
+
+    StateB = #state{
+            active = Emt,
+            down = Active,
+            backup = Backup
+            },
+    {reply, AnsB1, StateB1} = handle_call(get_pool, undefined, StateB),
+    {{value, 5}, _} = AnsB1,
+    {reply, AnsB2, StateB2} = handle_call(get_pool, undefined, StateB1),
+    {{value, 6}, _} = AnsB2,
+    {reply, AnsB3, StateB3} = handle_call(get_pool, undefined, StateB2),
+    {{value, 7}, _} = AnsB3,
+    {reply, AnsB4, StateB4} = handle_call(get_pool, undefined, StateB3),
+    {{value, 5}, _} = AnsB4,
+    
+    StateC = #state{
+            active = Emt,
+            down = Active,
+            backup = Emt
+            },
+    {reply, AnsC1, StateC1} = handle_call(get_pool, undefined, StateC),
+    {empty, _} = AnsC1,
+    {reply, AnsC2, StateC2} = handle_call(get_pool, undefined, StateC1),
+    {empty, _} = AnsC2,
+    {reply, AnsC3, StateC3} = handle_call(get_pool, undefined, StateC2),
+    {empty, _} = AnsC3,
+
+    ok.
+
+
+-endif.
 

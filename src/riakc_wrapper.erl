207b0de757314fb08d316e63579fe4eca08d4fb0
Paul Khusainov <alvelcom@gmail.com>
2013-02-02 12:09:44 +0400

    Added check_down to riak wrapper

--- src/riakc_wrapper.erl
+++ src/riakc_wrapper.erl
@@ -1,14 +1,21 @@
 % Like a monkey patching
--module(riakc_worker).
 -behaviour(poolboy_worker).
+-module(riakc_wrapper).
 
 -export([start_link/1, stop/1]).
+-export([check_down/1]).
 
 start_link(Args) ->
     Address = proplists:get_value(address, Args, "localhost"),
     Port = proplists:get_value(port, Args, 8087),
     Opts = proplists:get_value(opts, Args, []),
     riakc_pb_socket:start_link(Address, Port, Opts).
+
+check_down(Pid) ->
+    case riakc_pb_socket:ping(Pid) of
+        pong -> ok;
+        _    -> fail
+    end.
 
 stop(Pid) ->
     riakc_pb_socket:stop(Pid).

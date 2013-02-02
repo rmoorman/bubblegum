d467ab9b1af5f61fe3f0d36a48ada4326785ec1b
Paul Khusainov <alvelcom@gmail.com>
2013-02-02 12:12:35 +0400

    poolpool test worker now have poolpool behaviour

--- test/poolpool_test_worker.erl
+++ test/poolpool_test_worker.erl
@@ -1,21 +1,26 @@
 -module(poolpool_test_worker).
 -behaviour(gen_server).
--behaviour(poolboy_worker).
+-behaviour(poolpool).
 
--export([start_link/1]).
+-export([start_link/1, stop/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 -export([check_down/1]).
 -export([cookie/1]).
 
+%% API
 start_link(Args) ->
     gen_server:start_link(?MODULE, Args, []).
+
+stop(Pid) ->
+    gen_server:call(Pid, die).
 
 cookie(Id) ->
     gen_server:call(Id, get_state).
 
 check_down(_Id) -> ok.
 
+%% gen_server
 init(State) ->
     {ok, State}.
 

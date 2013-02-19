e5c7a15ef65806a90d18d1126349f1e115e59ee6
Paul Khusainov <alvelcom@gmail.com>
2013-02-19 18:16:18 +0400

    More complex type difinition for acl

--- lib/acl/include/acl.hrl
+++ lib/acl/include/acl.hrl
@@ -1,5 +1,9 @@
 
--type role_id() :: non_neg_integer() | binary(). 
+-type role_id() :: non_neg_integer(). 
+-type resource_id() :: non_neg_integer(). 
+-type action_name() :: binary().
+-type acl_action() :: {action_name(), [role_id()], [role_id()]}.
+
 -record(acl_role, {
         id :: role_id(),
         updated_by,
@@ -7,11 +11,12 @@
         member_of :: [role_id()]
         }).
 
+
 -record(acl_resource, {
         id :: resource_id(),
         updated_by,
         updated_at,
-        actions :: orddict:orddict()
+        actions :: [acl_action()]
         }).
 
 

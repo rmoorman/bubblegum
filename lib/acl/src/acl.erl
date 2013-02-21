4fe149e48f7bffb53ef891790a75bdf843d1a7eb
Paul Khusainov <alvelcom@gmail.com>
2013-02-21 09:13:07 +0400

    Added db-connection to acl

--- lib/acl/src/acl.erl
+++ lib/acl/src/acl.erl
@@ -6,6 +6,7 @@
               actions/0, verdict/0, verdicts/0,
               precedent/0, precedents/0]).
 
+%% Some useful types
 -type role_id() :: string().
 -type resource_id() :: string().
 
@@ -20,15 +21,17 @@
 
 -type answer() :: ok | {fail, atom()} | {fail, atom(), string()}.
 
+-type conn() :: term().
+
 %% @doc Ask for permission
--spec ask(role_id(), actions(), resource_id()) -> verdicts().
-ask(Role, Action, Resource) when is_atom(Action) ->
-    case ask(Role, [Action], Resource) of
+-spec ask(conn(), role_id(), actions(), resource_id()) -> verdicts().
+ask(C, Role, Action, Resource) when is_atom(Action) ->
+    case ask(C, Role, [Action], Resource) of
         [Ans] -> Ans;
         Error -> Error
     end;
 
-ask(Role, Actions, Resource) ->
+ask(C, Role, Actions, Resource) ->
     [allow || _ <- Actions]. % haha TODO
 
 
@@ -39,39 +42,39 @@ ask(Role, Actions, Resource) ->
 %%
 %% When resource have {Role, Action} pair, update_precedents remaps it
 %% to the specified Verdict.
--spec update_precedents(resource_id(), precedents()) -> answer().
-update_precedents(Resource, Precedents) -> {fail, todo}.
+-spec update_precedents(conn(), resource_id(), precedents()) -> answer().
+update_precedents(C, Resource, Precedents) -> {fail, todo}.
 
 
 %% @doc Fully rewrites a precedent list for the given resource
 %%
 %% Erases old precedent list and create a new one based on Precedents.
--spec set_precedents(resource_id(), precedents()) -> answer().
-set_precedents(Resource, Precedents) -> {fail, todo}.
+-spec set_precedents(conn(), resource_id(), precedents()) -> answer().
+set_precedents(C, Resource, Precedents) -> {fail, todo}.
 
 
 %% @doc Gets all precedents for the given resource
--spec get_precedents(resource_id()) -> precedents().
-get_precedents(Resource) -> [].
+-spec get_precedents(conn(), resource_id()) -> precedents().
+get_precedents(C, Resource) -> [].
 
 
 %% @doc Gets primary roles for the given role
--spec get_roles(role_id()) -> [role_id()].
-get_roles(Role) -> [].
+-spec get_roles(conn(), role_id()) -> [role_id()].
+get_roles(C, Role) -> [].
 
 
 %% @doc Gets all roles for the given role
--spec get_all_roles(role_id()) -> [role_id()].
-get_all_roles(Role) -> [].
+-spec get_all_roles(conn(), role_id()) -> [role_id()].
+get_all_roles(C, Role) -> [].
 
 
 %% @doc Incrementally update roles for the given role
--spec update_roles(role_id(), [role_id()]) -> answer().
-update_roles(Role, Roles) -> {fail, todo}.
+-spec update_roles(conn(), role_id(), [role_id()]) -> answer().
+update_roles(C, Role, Roles) -> {fail, todo}.
 
 
 %% @doc Rewrite roles for the given role
--spec set_roles(role_id(), [role_id()]) -> answer().
-set_roles(Role, Roles) -> {fail, todo}.
+-spec set_roles(conn(), role_id(), [role_id()]) -> answer().
+set_roles(C, Role, Roles) -> {fail, todo}.
 
 %% to be continued...

639d68c96058a98006569e6568d19faeddd38a53
Paul Khusainov <alvelcom@gmail.com>
2013-02-25 16:14:33 +0400

    Make default postgres params

--- lib/pg_pool/src/pg_pool_worker.erl
+++ lib/pg_pool/src/pg_pool_worker.erl
@@ -4,10 +4,7 @@
 -export([start_link/1]).
 
 start_link(Args) ->
-    Hostname = proplists:get_value(hostname, Args),
-    Database = proplists:get_value(database, Args),
-    Username = proplists:get_value(username, Args),
-    Password = proplists:get_value(password, Args),
-    {ok, _} = pgsql:connect(Hostname, Username, Password, [
-                {database, Database}
-                ]).
+    Hostname = proplists:get_value(hostname, Args, "127.0.0.1"),
+    Username = proplists:get_value(username, Args, os:getenv("USER")),
+    Password = proplists:get_value(password, Args, ""),
+    {ok, _} = pgsql:connect(Hostname, Username, Password, Args).

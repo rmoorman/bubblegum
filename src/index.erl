0d7abb31a26acda0d3bb3f5e47632c580ff00932
Paul Khusainov <alvelcom@gmail.com>
2013-03-03 13:13:48 +0400

    Bootstrapping index

--- src/index.erl
+++ src/index.erl
@@ -8,32 +8,25 @@ main() -> #template { file="./priv/templates/bare.html" }.
 title() -> "Welcome to Nitrogen".
 
 body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
+    #panel {
+        class = "container",
+        body = 
+        #panel{
+            class ="hero-unit",
+            body=inner_body()}}.
 
 inner_body() -> 
     [
-        #h1 { text="Welcome to Nitrogen" },
-        #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{}, 	
-        #button { id=button, text="Click me!", postback=click },
-		#p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        ",
-		#p{},
-		"
-		<b>Want to see the ",#link{text="Sample Nitrogen jQuery Mobile Page",url="/mobile"},"?</b>
-		"
+        #h1{ text="Funtester" },
+        #p{text = "If you can see this page, then funtester is up and running."},
+        #p{text = "Click the button below to test nitrogen postbacks."},
+        #button{ id=button, class="btn btn-primary btn-large", text="Click me!", postback=click }
+
     ].
 	
 event(click) ->
     wf:replace(button, #panel { 
+        class="alert alert-success",
         body="You clicked the button!", 
         actions=#effect { effect=highlight }
     }).

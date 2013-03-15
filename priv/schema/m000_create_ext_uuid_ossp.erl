3863e62233c2d5a80781a0d007b244352874e659
Paul Khusainov <alvelcom@gmail.com>
2013-03-15 22:01:19 +0400

    Add uuid-ossp to postgres

---
+++ priv/schema/m000_create_ext_uuid_ossp.erl
@@ -0,0 +1,8 @@
+-module(m000_create_ext_uuid_ossp).
+-behaviour(migrator).
+
+-export([upgrade/1
+        ,downgrade/1]).
+
+upgrade(C) -> model_migrate:migration("CREATE EXTENSION \"uuid-ossp\"").
+downgrade(C) -> model_migrate:migration("DROP EXTENSION IF EXIST \"uuid-ossp\"").

1da18b59c9c74f9b2ed824091ae4440341e448c5
Paul Khusainov <alvelcom@gmail.com>
2013-03-16 20:26:05 +0400

    Add routing

--- include/web.hrl
+++ include/web.hrl
@@ -12,13 +12,21 @@
 -define(routes, 
         [% Modules
          {"/",          web_index}
+
         ,{"/signin",    web_signin}
         ,{"/signup",    web_signup}
         ,{"/logout",    web_logout}
+
         ,{"/profile",   web_profile}
         ,{"/profiles",  web_profiles}
+
         ,{"/contest",   web_contest}
         ,{"/contests",  web_contests}
+
+        ,{"/problem",   web_problem}
+        ,{"/problem/edit", web_problem_edit}
+        ,{"/problems/new", web_problem_edit}
+        ,{"/problems",  web_problems}
 
          % Some static
         ,{"/bootstrap", static_file}
@@ -28,3 +36,17 @@
         ,{"/images",    static_file}
         ]).
 
+
+%% Lazy load with process dictionary as storage
+-define(l(Function), case get({lazy, Function}) of
+        undefined -> 
+            Value = Function(),
+            put({lazy, Function}, {ok, Value}),
+            Value;
+        {ok, Value} ->
+            Value
+    end).
+                
+%% Bad url generator
+-define(url1(Mod), ["/", atom_to_list(Mod)]).
+-define(url2(Mod, Param), ["/", atom_to_list(Mod), "/", Param]).

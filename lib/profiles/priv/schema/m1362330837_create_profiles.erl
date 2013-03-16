b09301a4c1ab8e44d3acb67273eca849159ae9de
Paul Khusainov <alvelcom@gmail.com>
2013-03-16 13:24:51 +0400

    Fix schema for profiles

--- lib/profiles/priv/schema/m1362330837_create_profiles.erl
+++ lib/profiles/priv/schema/m1362330837_create_profiles.erl
@@ -5,10 +5,12 @@
 upgrade(_C) ->
     model_migrate:create_kv_table_uuid(profiles),
     model_migrate:create_kv_table_string(profiles_emails),
-    model_migrate:create_kv_table_string(profiles_logins).
+    model_migrate:create_kv_table_string(profiles_logins),
+    model_migrate:create_stream_table_uuid(profiles_stream).
 
 downgrade(_C) ->
     model_migrate:drop_kv_table(profiles),
     model_migrate:drop_kv_table(profiles_emails),
-    model_migrate:drop_kv_table(profiles_logins).
-
+    model_migrate:drop_kv_table(profiles_logins),
+    model_migrate:drop_stream_table(profiles_stream).
+    

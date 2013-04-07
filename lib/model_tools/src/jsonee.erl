b0548fa85ef079fa7a89b708ea31dcfa5dc6866f
Paul Khusainov <alvelcom@gmail.com>
2013-04-07 18:18:32 +0400

    Merge records in jsonee

--- lib/model_tools/src/jsonee.erl
+++ lib/model_tools/src/jsonee.erl
@@ -37,6 +37,7 @@
         ,from_eep18/2
         ,encode/2
         ,decode/2
+        ,merge/4
         ]).
 
 -include("model.hrl").
@@ -135,6 +136,16 @@ from_eep18({V}, {Fields, FormatsDict}) ->
     list_to_tuple(List);
 from_eep18(undefined, _) -> undefined.
 
+
+%% Merge
+%% Merge by fact has nothing to json,
+%% but I don't know about better module where I can place It.
+merge(Function, List, R1, R2) ->
+    [Tag|L1] = tuple_to_list(R1),
+    [Tag|L2] = tuple_to_list(R2),
+    Merged = lists:zipwith3(Function, List, L1, L2),
+    tuple_to_list([Tag|Merged]).
+                      
 %% Tests
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").

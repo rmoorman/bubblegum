f984ebdd6e770084a686b3ebf8642ba4b09eaeca
Paul Khusainov <alvelcom@gmail.com>
2013-04-07 18:40:10 +0400

    Cowboy routes

--- src/cowboy_wrapper.erl
+++ src/cowboy_wrapper.erl
@@ -4,10 +4,15 @@
 
 start() ->
     Dispatch = cowboy_router:compile([
-        {'_', [{"/users", users_handler, []}
+        {'_', [{"/user", users_handler, []}
+              ,{"/users", users_handler, []}
               ,{"/user/:user_id", users_handler, []}
+              ,{"/problems", problems_handler, []}
+              ,{"/problem/:problem_id", problems_handler, []}
               ]}]),
     {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
                 {env, [{dispatch, Dispatch}]}]).
 
 
+priv() ->
+    code:priv_dir(funtester).

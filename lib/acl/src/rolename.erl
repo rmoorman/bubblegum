4a4a4cbd430840d6f0e765ba81ba44c5b9703b51
Paul Khusainov <alvelcom@gmail.com>
2013-05-26 18:41:40 +0400

    Revert "Support of atoms in rolenames"
    This reverts commit a04c32e0a25dccd9d6876ac3949600424d49bbfc.

--- lib/acl/src/rolename.erl
+++ lib/acl/src/rolename.erl
@@ -11,11 +11,9 @@
 -define(r2n, acl_rolename).
 -define(n2r, acl_namerole).
 
--define(tostr(Q), jsonee:to_eep18(Q, string)).
-
 add(Role, Name) ->
     ?u:create(Role, Name, string, ?r2n),
-    ?u:create(?tostr(Name), Role, uuid, ?n2r).
+    ?u:create(Name, Role, uuid, ?n2r).
 
 delete(Role) ->
     {ok, Name} = get_name(Role),
@@ -23,11 +21,11 @@ delete(Role) ->
     ?u:delete(Name, ?n2r).
 
 get_role(Name) ->
-    ?u:read(?tostr(Name), uuid, ?n2r).
+    ?u:read(Name, uuid, ?n2r).
 
 get_name(Role) ->
     ?u:read(Role, string, ?r2n).
 
 exists(Name) ->
-    ?u:exists(?tostr(Name), ?n2r).
+    ?u:exists(Name, ?n2r).
 
